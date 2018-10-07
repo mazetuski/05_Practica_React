71f59933aada91ba81d8113ddf3e3b599785db31
Miguel <miguel@sopinet.com>
2018-10-07 16:36:57 +0200

    Adding router

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from "./store";
 import {Provider} from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
     <Provider store={store}>
-      <App/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={App}/>
+          <Route exact path="/logout" component={App}/>
+        </Switch>
+      </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
